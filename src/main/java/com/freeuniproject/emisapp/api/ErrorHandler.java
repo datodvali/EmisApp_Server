0f294ab3f30b410f85fde9d4f26ca6ffeb715209
dato <dato11235813@gmail.com>
2023-08-25 22:14:11 +0400

    controllerAdvice added

---
+++ src/main/java/com/freeuniproject/emisapp/api/ErrorHandler.java
@@ -0,0 +1,16 @@
+package com.freeuniproject.emisapp.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex) {
+        return new ResponseEntity<>("There happened a terrible errorc: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
